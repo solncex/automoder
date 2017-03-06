5d624d5bc8d41dc9e31d43c2cd4d0706ce2fed1c
ilham <ilhamx@yandex.ru>
2017-03-06 07:52:33 +0300

    fix

--- public/params.php
+++ public/params.php
@@ -3,13 +3,13 @@ if (!defined('ROOT_PATH')) {
 define('ROOT_PATH', dirname(__DIR__));
 }
 if (!defined('MYSQL_HOST')) {
-define('MYSQL_HOST', 'localhost');
+define('MYSQL_HOST', '');
 }
 if (!defined('MYSQL_USER')) {
-define('MYSQL_USER', 'root');
+define('MYSQL_USER', '');
 }
 if (!defined('MYSQL_PASSWORD')) {
-define('MYSQL_PASSWORD', '111');
+define('MYSQL_PASSWORD', '');
 }
 if (!defined('MYSQL_DB')) {
 define('MYSQL_DB', 'adv-avtomoder');
